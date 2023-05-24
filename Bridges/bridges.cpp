d00a12c7105f6915644a999b3f56fc371b6b04f2
DaniilR <daniil.r4@yandex.ru>
2023-05-24 20:01:13 +0300

    Complete Bridges!

--- Bridges/bridges.cpp
+++ Bridges/bridges.cpp
@@ -1,83 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <algorithm>
-#include <unordered_set>
+#include <map>
 
 using std::vector;
-struct Edge {
-  int start = -1;
-  int finish = -1;
-  int number = -1;
-  bool is_parallel = false;
-};
-
-struct pair_hash {
-  inline std::size_t operator()(const std::pair<int, int> &v) const {
-    return v.first * 31 + v.second;
-  }
-};
 
 enum Color { WHITE, GREY, BLACK };
+
 struct Vertex {
-  int index = -1;
+  int index = 0;
   int time_in = 0;
   int time_up = 0;
   Color color = WHITE;
-  std::unordered_set<int> edges;
-  static int time;
 };
-
-int Vertex::time;
 
 class Graph {
  public:
-  explicit Graph(int n, int e) {
-    Vertex::time = 0;
-    vertexes_ = vector<Vertex>(n);
-    edges_ = vector<Edge>(e);
+  explicit Graph(int v) {
+    vertexes_ = vector<Vertex>(v);
+    edges_ = vector<vector<int>>(v);
   }
 
-  void FindBridges() {
+  std::set<int> FindBridges() {
     for (auto &vertex : vertexes_) {
       if (vertex.color == WHITE) {
         DFS(vertex, 0);
       }
     }
-    std::vector<int> ans;
-    for (auto x : bridges) {
-      for (auto y : edges_) {
-        if (y.start == x.first && y.finish == x.second && !y.is_parallel) {
-          ans.push_back(y.number);
-        }
-      }
-    }
-    std::cout << ans.size() << '\n';
-    for(auto x : ans) {
-      std::cout << x << '\n';
-    }
+
+    return bridges_;
   }
 
   friend std::istream &operator>>(std::istream &is, Graph &g);
 
  private:
   vector<Vertex> vertexes_;
-  std::set<std::pair<int, int>> bridges;
-  int edges_amount = 0;
-  std::vector<Edge> edges_;
+  vector<vector<int>> edges_;
+  std::set<int> bridges_;
+  std::map<std::pair<int, int>, int> edges_dict_;
+  int edges_counter_ = 0;
+  int time_ = 0;
 
-  std::unordered_set<std::pair<int, int>, pair_hash> edges_set_;
-  // Methods
   void DFS(Vertex &curr, int ancestor) {
     curr.color = GREY;
-    curr.time_in = curr.time_up = ++Vertex::time;
+    curr.time_in = curr.time_up = ++time_;
 
-    for (auto x : curr.edges) {
+    for (auto x : edges_[curr.index]) {
       if (x == ancestor) {
-        if (bridges.count({std::min(curr.index, ancestor), std::max(curr.index, ancestor)})) {
-          bridges.erase({std::min(curr.index, ancestor), std::max(curr.index, ancestor)});
-        }
-        continue;//Добавить проверку на наличие в bridges чтобы удалить если что
+        continue;
       }
 
       if (vertexes_[x].color == GREY) {
@@ -88,9 +58,8 @@ class Graph {
         DFS(vertexes_[x], curr.index);
         curr.time_up = std::min(curr.time_up, vertexes_[x].time_up);
 
-        if (curr.time_in < vertexes_[x].time_up) {
-
-          bridges.insert({std::min(curr.index, x), std::max(curr.index, x)});
+        if (curr.time_in < vertexes_[x].time_up && edges_dict_[{curr.index, vertexes_[x].index}] != -1) {
+          bridges_.insert(edges_dict_[{curr.index, vertexes_[x].index}]);
         }
       }
     }
@@ -105,17 +74,24 @@ std::istream &operator>>(std::istream &is, Graph &g) {
   --begin;
   --end;
 
-  bool is_parallel = g.edges_set_.count({std::min(begin, end), std::max(begin, end)});
-  ++g.edges_amount;
-  g.edges_set_.insert({begin, end});
-  g.edges_[g.edges_amount] = {std::min(begin, end), std::max(begin, end), g.edges_amount};
-  g.edges_[g.edges_amount].is_parallel = is_parallel;
+  if (begin == end) {
+    // Loop
+    return is;
+  }
 
+  if (g.edges_dict_.count(std::pair(begin, end)) > 0) {
+    // Multiple edge
+    g.edges_dict_[{begin, end}] = -1;
+    g.edges_dict_[{end, begin}] = -1;
+    return is;
+  }
 
   g.vertexes_[begin].index = begin;
   g.vertexes_[end].index = end;
-  g.vertexes_[begin].edges.insert(end);
-  g.vertexes_[end].edges.insert(begin);
+  g.edges_[begin].push_back(end);
+  g.edges_[end].push_back(begin);
+  g.edges_dict_[{begin, end}] = ++g.edges_counter_;
+  g.edges_dict_[{end, begin}] = g.edges_counter_;
 
   return is;
 }
@@ -123,12 +99,18 @@ std::istream &operator>>(std::istream &is, Graph &g) {
 int main() {
   int v, e;
   std::cin >> v >> e;
-  Graph g(v, e);
+  Graph g(v);
 
   for (int i = 0; i < e; ++i) {
     std::cin >> g;
   }
 
-  g.FindBridges();
+  std::set<int> bridges = g.FindBridges();
+
+  std::cout << bridges.size() << '\n';
+  for (auto bridge : bridges) {
+    std::cout << bridge << '\n';
+  }
+
   return 0;
 }
